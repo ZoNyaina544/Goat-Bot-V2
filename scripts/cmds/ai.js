31c6dc3ede37a4f504ec3224ac1c4708330bab4f
seijuro556 <152220762+seijuro556@users.noreply.github.com>
2024-03-29 20:43:53 +0300

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -3,35 +3,6 @@ const axios = require('axios');
 const GPT_API_URL = 'https://sandipapi.onrender.com/gpt';
 const PREFIXES = ['ai'];
 
-function transformText(text) {
-  const replacements = {
-    a: "𝚊",
-    b: "𝚋",
-    c: "𝚌",
-    d: "𝚍",
-    e: "𝚎",
-    f: "𝚏",
-    g: "𝚐",
-    h: "𝚑",
-    i: "𝚒",
-    j: "𝚓",
-    k: "𝚔",
-    l: "𝚕",
-    m: "𝚖",
-    n: "𝚗",
-    o: "𝚘",
-    p: "𝚙",
-    q: "𝚚", 
-    𝚛: "𝚛", 
-    s: "𝚜",
-    t: "𝚝",
-    u: "𝚞",
-    v: "𝚟",
-    w: "𝚠",
-    x: "𝚡",
-    y: "𝚢",
-    z: "𝚣",
-    
 module.exports = {
   config: {
     name: "ai",
@@ -86,4 +57,4 @@ async function getGPTResponse(prompt) {
 
   const response = await axios.get(`${GPT_API_URL}?prompt=${encodeURIComponent(prompt)}`);
   return response.data.answer;
-    }
+}
